95aaf81c620548a3fac6a850304ebfe77c9453c3
hunmac9 <hunmac9@gmail.com>
2025-04-04 07:08:47 +0000

    added pdf viewer

--- shared/editor/commands/insertFiles.ts
+++ shared/editor/commands/insertFiles.ts
@@ -1,5 +1,4 @@
-import * as Sentry from "@sentry/react";
-import { EditorView } from "prosemirror-view";
+import * as Sentry from "@sentry/react"; import { EditorView } from "prosemirror-view";
 import { toast } from "sonner";
 import { v4 as uuidv4 } from "uuid";
 import type { Dictionary } from "~/hooks/useDictionary";
